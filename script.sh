c3be30141e80c9a5c2cd6f00fd75fd9ffa525aee
naveenkum1r <35126225+naveenkum1r@users.noreply.github.com>
2021-01-18 20:32:25 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+printf "Installing Node keep Patience... " >&2
+{
+curl -sL https://deb.nodesource.com/setup_10.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt install nodejs
+} &> /dev/null &&
+printf "\nSetup Complete " >&2 ||
+printf "\nError Occured " >&2
+cd rdpcode
+npm install mongodb axios cloudinary adm-zip md5-file
+node dandu.js
